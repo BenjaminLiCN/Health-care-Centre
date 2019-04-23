fbc4eb58768739a20386c9f5cc27e7e10e144125
BenjaminLiCN <li.benjamincn@gmail.com>
2019-04-23 15:04:07 +1000

    First workable code

--- src/test/java/com/benjamin/wte/hccApplicationTests.java
+++ src/test/java/com/benjamin/wte/hccApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WteApplicationTests {
+public class hccApplicationTests {
 
 	@Test
 	public void contextLoads() {
